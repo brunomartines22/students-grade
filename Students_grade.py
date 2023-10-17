4265550d304b557baf26d54df3377236b578af11
brunomartines22 <147097812+brunomartines22@users.noreply.github.com>
2023-10-17 12:52:35 -0600

    Create Students_grade.py

---
+++ Students_grade.py
@@ -0,0 +1,13 @@
+avarage_grade = int(input("Enter your avarage grade: "))
+clasroom_participation = str(input("Enter your Clasroom Participation (yes/no) "))
+if avarage_grade > 75 and clasroom_participation == "yes":
+    print("you have a good academic standing")
+elif avarage_grade < 60 and clasroom_participation =="no":
+    print("you have a bad academic standing")
+project_score = int(input("Enter your project score: "))
+if project_score > 90:
+  print("You have a distingtion")
+else:
+  print("you dont have any distingtion")
+
+link : https://replit.com/join/nryliflwsm-brunoarzate2
